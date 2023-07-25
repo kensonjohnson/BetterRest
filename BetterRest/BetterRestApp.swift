6365b2fe981bf0943624c2607724ec11bfc9209b
Kenson Johnson <kenson.johnson@outlook.com>
2023-07-25 12:57:40 -0500

    Generated project files

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kenson Johnson on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
